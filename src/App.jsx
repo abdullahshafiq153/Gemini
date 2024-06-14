ce62fd8eee104433fc59527f78a5932586d5bedd
abdullahshafiq153 <abdullahshafiq153@gmail.com>
2024-06-15 03:52:09 +0500

    Main container-> navbar, greetings, cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Sidebar from "./components/Sidebar/Sidebar";
-// import Main from "./components/Main/Main";
+import Main from "./components/Main/Main";
 
 const App = () => {
   return (
     <>
       <Sidebar />
-      {/* <Main /> */}
+      <Main />
     </>
   );
 };
